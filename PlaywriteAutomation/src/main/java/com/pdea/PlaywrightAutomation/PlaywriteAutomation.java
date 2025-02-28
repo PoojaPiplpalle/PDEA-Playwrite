9fb0cc849ee8abfd743489a37babe142d8fb3ee0
HP <HP@DESKTOP-G761TD1>
2025-02-28 12:55:37 +0530

    Te

--- PlaywriteAutomation/src/main/java/com/pdea/PlaywrightAutomation/PlaywriteAutomation.java
+++ PlaywriteAutomation/src/main/java/com/pdea/PlaywrightAutomation/PlaywriteAutomation.java
@@ -2,14 +2,13 @@ package com.pdea.PlaywrightAutomation;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import com.google.gson.JsonObject;
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
@@ -18,141 +17,142 @@ import com.microsoft.playwright.options.BoundingBox;
 import com.microsoft.playwright.options.SelectOption;
 
 public class PlaywriteAutomation {
-	  Playwright playwright;
-	    Browser browser;
-	    String caps;
-	    String cdpUrl;
-	    Page page;
-	    SoftAssert softAssert = new SoftAssert();
 
-	    @BeforeMethod
-	    public void setup() throws UnsupportedEncodingException {
-	    	//hyperexecute.exe --user nitin_ghugare --key GB0X4pJZeWJGoh8vnRcG7vdTDbe02AapWYBOS50udqFCPIsjKp --config .Hyperexecute
-	        JsonObject capabilities = new JsonObject();
-	        JsonObject ltOptions = new JsonObject();
-	        String user = "poojapiplpalle";
-	        String accessKey = "zfqXEXwkGxj3WxlafcQtgXFnZCypNFT7AdZKKLTMInueh0HQJ5";
+    Playwright playwright;
+    Browser browser;
+    String caps;
+    String cdpUrl;
+    Page page;
+    SoftAssert softAssert = new SoftAssert();
 
-	        ltOptions.addProperty("name", "Playwright Automation");
-	        ltOptions.addProperty("build", "Playwright Exam Assignment");
-	        ltOptions.addProperty("user", user);
-	        ltOptions.addProperty("accessKey", accessKey);
-	        ltOptions.addProperty("visual", true);
-	        ltOptions.addProperty("video", true);
-	        ltOptions.addProperty("platformName", "Windows 10");  
-	        ltOptions.addProperty("browserName", "chrome"); 
-	        ltOptions.addProperty("browserVersion", "latest"); 
-	        
-	        capabilities.add("LT:Options", ltOptions);
+    @BeforeMethod
+    public void setup() throws UnsupportedEncodingException {
+    	//hyperexecute.exe --user poojapiplpalle --key zfqXEXwkGxj3WxlafcQtgXFnZCypNFT7AdZKKLTMInueh0HQJ5 --config ./Hyperexecute.yaml
+    	//set LT_USERNAME="poojapiplpalle"
+    //	set LT_ACCESS_KEY="zfqXEXwkGxj3WxlafcQtgXFnZCypNFT7AdZKKLTMInueh0HQJ5"
+        JsonObject capabilities = new JsonObject();
+        JsonObject ltOptions = new JsonObject();
+        String user = "poojapiplpalle";
+        String accessKey = "zfqXEXwkGxj3WxlafcQtgXFnZCypNFT7AdZKKLTMInueh0HQJ5";
 
-	        playwright = Playwright.create();
-	        BrowserType chromium = playwright.chromium();
-	        // Launch the browser
-	        caps = URLEncoder.encode(capabilities.toString(), "utf-8");
-	        cdpUrl = "wss://cdp.lambdatest.com/playwright?capabilities=" + caps;
-	        browser = playwright.chromium().connect(cdpUrl);
+        ltOptions.addProperty("name", "Playwright Automation");
+        ltOptions.addProperty("build", "Playwright Exam Assignment");
+        ltOptions.addProperty("user", user);
+        ltOptions.addProperty("accessKey", accessKey);
+        ltOptions.addProperty("visual", true);
+        ltOptions.addProperty("video", true);
+        ltOptions.addProperty("platformName", "Windows 10");  
+        ltOptions.addProperty("browserName", "chrome"); 
+        ltOptions.addProperty("browserVersion", "latest"); 
+        
+        capabilities.add("LT:Options", ltOptions);
 
-	        page = browser.newPage();
-	        System.out.println("Launch chrome");
-	        
-	        
-	    }
+        playwright = Playwright.create();
+        BrowserType chromium = playwright.chromium();
+        // Launch the browser
+        caps = URLEncoder.encode(capabilities.toString(), "utf-8");
+        cdpUrl = "wss://cdp.lambdatest.com/playwright?capabilities=" + caps;
+        browser = playwright.chromium().connect(cdpUrl);
 
-	    @Test(priority = 1)
-	    public void test_scenario1() throws Exception {
-	        page.navigate("https://www.lambdatest.com/selenium-playground");
-	        page.click("//*[text()='Simple Form Demo']");
+        page = browser.newPage();
+    }
 
-	        softAssert.assertTrue(page.url().contains("simple-form-demo"),
-	                "Failed : The page URL does not contain 'simple-form-demo'");
+    @Test(priority = 1)
+    public void test_scenario1() throws Exception {
+        page.navigate("https://www.lambdatest.com/selenium-playground");
+        page.click("//*[text()='Simple Form Demo']");
 
-	        String message = "Welcome to LambdaTest";
+        softAssert.assertTrue(page.url().contains("simple-form-demo"),
+                "Failed : The page URL does not contain 'simple-form-demo'");
 
-	        page.fill("#user-message", message);
+        String message = "Welcome to LambdaTest";
 
-	        page.click("//*[@id='showInput']");
+        page.fill("#user-message", message);
 
-	        String displayedMessage = page.innerText("#message");
+        page.click("//*[@id='showInput']");
 
-	        softAssert.assertEquals(displayedMessage, message,
-	                "Failed : The message displayed does not match with the one that was entered");
-	        System.out.println(displayedMessage);
-	        softAssert.assertAll();
-	    }
+        String displayedMessage = page.innerText("#message");
 
-	    @Test(priority = 2)
-	    public void test_scenario2() throws Exception {
-	        page.navigate("https://www.lambdatest.com/selenium-playground");
+        softAssert.assertEquals(displayedMessage, message,
+                "Failed : The message displayed does not match with the one that was entered");
+        System.out.println(displayedMessage);
+        softAssert.assertAll();
+    }
 
-	        page.waitForSelector("text=Drag & Drop Sliders");
+    @Test(priority = 2)
+    public void test_scenario2() throws Exception {
+        page.navigate("https://www.lambdatest.com/selenium-playground");
 
-	        page.click("text=Drag & Drop Sliders");
+        page.waitForSelector("text=Drag & Drop Sliders");
 
-	        Locator slider = page.locator("//input[@value=15]");
-	        BoundingBox initialBBox = slider.boundingBox();
+        page.click("text=Drag & Drop Sliders");
 
-	        // Dragging the slider to 95
-	        slider.hover();
-	        page.mouse().move(initialBBox.x + initialBBox.width / 2, initialBBox.y + initialBBox.height / 2); // Hover at
-	        page.mouse().down();
-	        page.mouse().move(initialBBox.x + 465, initialBBox.y); 
-	        page.mouse().up();
+        Locator slider = page.locator("//input[@value=15]");
+        BoundingBox initialBBox = slider.boundingBox();
 
-	        Locator outputElement = page.locator("#rangeSuccess");
- 
-	        String outputText = outputElement.textContent();
+        // Dragging the slider to 95
+        slider.hover();
+        page.mouse().move(initialBBox.x + initialBBox.width / 2, initialBBox.y + initialBBox.height / 2); // Hover at
+        page.mouse().down();
+        page.mouse().move(initialBBox.x + 465, initialBBox.y); 
+        page.mouse().up();
 
-	        softAssert.assertEquals(outputText, "95", "Slider value does not equal 95");
-	        softAssert.assertAll();
-	    }
+        Locator outputElement = page.locator("#rangeSuccess");
 
-	    @Test(priority = 3)
-	    public void test_scenario3() throws Exception {
-	        page.navigate("https://www.lambdatest.com/selenium-playground");
+        String outputText = outputElement.textContent();
 
-	        page.locator("text=Input Form Submit").click();
+        softAssert.assertEquals(outputText, "95", "Slider value does not equal 95");
+        softAssert.assertAll();
+    }
 
-	        Locator submitBtn = page.locator("//button[text()='Submit']");
-	        submitBtn.click();
+    @Test(priority = 3)
+    public void test_scenario3() throws Exception {
+        page.navigate("https://www.lambdatest.com/selenium-playground");
 
-	        String validationMsg = page.locator("//input[@name='name']").getAttribute("validationMessage");
-	        String expectedErrorMsg = "Please fill out this field.";
-	        softAssert.assertEquals(validationMsg, expectedErrorMsg, "Error message not as expected");
+        page.locator("text=Input Form Submit").click();
 
-	        page.fill("#name", "Nitin Ghugare");
+        Locator submitBtn = page.locator("//button[text()='Submit']");
+        Thread.sleep(1000);
+        submitBtn.click();
 
-	        page.fill("//input[@placeholder='Email']", "nitinghugare@gmail.com");
+        String validationMsg = page.locator("//input[@name='name']").getAttribute("validationMessage");
+        String expectedErrorMsg = "Please fill out this field.";
+        softAssert.assertEquals(validationMsg, expectedErrorMsg, "Error message not as expected");
 
-	        page.fill("//input[@placeholder='Password']", "Mumbai@23");
+        page.fill("#name", "Pooja Piplpalle");
 
-	        page.fill("//input[@placeholder='Company']", "Persistent Systems");
+        page.fill("//input[@placeholder='Email']", "poojaranip14@gmail.com");
 
-	        page.fill("#websitename", "www.persistent.com");
+        page.fill("//input[@placeholder='Password']", "Mumbai@23");
 
-	        page.selectOption("select[name='country']", new SelectOption().setLabel("United States"));
+        page.fill("//input[@placeholder='Company']", "Persistent Systems");
 
-	        page.fill("#inputCity", "New York City");
+        page.fill("#websitename", "www.persistent.com");
 
-	        page.fill("#inputAddress1", "Test road");
+        page.selectOption("select[name='country']", new SelectOption().setLabel("United States"));
 
-	        page.fill("#inputAddress2", "Test city");
+        page.fill("#inputCity", "New York City");
 
-	        page.fill("//input[@placeholder='State']", "Texas");
+        page.fill("#inputAddress1", "Test road");
 
-	        page.fill("//input[@name='zip']", "213067");
+        page.fill("#inputAddress2", "Test city");
 
-	        submitBtn.click();
+        page.fill("//input[@placeholder='State']", "Texas");
 
-	        softAssert.assertTrue(
-	                page.locator("text=Thanks for contacting us, we will get back to you shortly.").isVisible(),
-	                "Failed : Success message is not visible");
+        page.fill("//input[@name='zip']", "213067");
 
-	        softAssert.assertAll();
-	    }
+        submitBtn.click();
 
-	    @AfterMethod
-	    public void quitDriver() {
-	        browser.close();
-	        playwright.close();
-	    }
+        softAssert.assertTrue(
+                page.locator("text=Thanks for contacting us, we will get back to you shortly.").isVisible(),
+                "Failed : Success message is not visible");
+
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        browser.close();
+        playwright.close();
+    }
 }
